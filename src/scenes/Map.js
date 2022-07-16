1736aab9f92085ae352bf36fbf53fbf099ff6511
Daniel Whiffing <danielwhiffing@gmail.com>
2022-07-16 18:12:33 -0400

    Adds basic map scrolling

--- src/scenes/Map.js
+++ src/scenes/Map.js
@@ -24,16 +24,33 @@ export default class extends Phaser.Scene {
         .on('pointerdown', () => this.clickNode(node, i))
       return { ...node, sprite, index: i }
     })
+
+    this.input.on('pointermove', (p) => {
+      if (this.startY) {
+        this.cameras.main.scrollY = this.startScroll + (this.startY - p.y)
+      }
+    })
+
+    this.input.on('pointerdown', (p) => {
+      this.startY = p.y
+      this.startScroll = this.cameras.main.scrollY
+    })
+
+    this.input.on('pointerup', (p) => {
+      this.startY = null
+      this.startScroll = null
+    })
     const x = this.width / 2
     const y = 380 - this.registry.values.levelIndex * 100
     this.player = this.add.sprite(x, y, 'sheet', 'pawn.png').setOrigin(0.5)
+    this.cameras.main.centerOnY(this.player.y)
 
     // // launch remove die screen
     // this.registry.values.levelIndex = 1
     // this.clickNode(this.nodes[1], 1)
 
     // autostart first battle
-    this.clickNode(this.nodes[0], 0)
+    // this.clickNode(this.nodes[0], 0)
 
     // // launch add die screen
     // this.scene.launch('Dice', { mode: 'add' })
