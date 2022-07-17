7df367482076daab3aa3f838c6ea3db295f33d43
Daniel Whiffing <danielwhiffing@gmail.com>
2022-07-17 12:41:38 -0400

    Adds fade to menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -52,19 +52,22 @@ export default class extends Phaser.Scene {
     this.scene.start('Help')
   }
   startGame = () => {
-    this.registry.values.lastX = 1
-    this.registry.values.deck = [...INITIAL_DECK].map((d, i) => ({
-      ...d,
-      index: i,
-    }))
-    this.registry.values.playerStats = {
-      drawCount: 0,
-      str: 0,
-      dex: 0,
-      hp: STATS.player.hp,
-    }
-    this.registry.values.activePile = []
-    this.registry.values.discardPile = []
-    this.scene.start('Map')
+    this.cameras.main.fadeOut(600)
+    this.time.delayedCall(600, () => {
+      this.registry.values.lastX = 1
+      this.registry.values.deck = [...INITIAL_DECK].map((d, i) => ({
+        ...d,
+        index: i,
+      }))
+      this.registry.values.playerStats = {
+        drawCount: 0,
+        str: 0,
+        dex: 0,
+        hp: STATS.player.hp,
+      }
+      this.registry.values.activePile = []
+      this.registry.values.discardPile = []
+      this.scene.start('Map', { fade: true })
+    })
   }
 }
